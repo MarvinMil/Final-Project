940877279d5d7aae09b1767b2c45a0fb77c3a8c1
MarvinMil <92934497+MarvinMil@users.noreply.github.com>
2025-08-13 22:00:48 +0200

    Delete webgis_uhi_final_en_visible directory

--- webgis_uhi_final_en_visible/Start App (Windows).bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-if not exist .venv (
-  py -3 -m venv .venv
-)
-call .venv\Scripts\activate
-python -m pip install --upgrade pip wheel >nul 2>&1
-pip install -r requirements.txt
-python app.py
